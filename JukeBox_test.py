220f758b0af8c72382d6cff7d0e2650c4423a118
DK0310 <lekhangdoan3@gmail.com>
2025-08-04 20:45:48 +0700

    test added

--- JukeBox_test.py
+++ JukeBox_test.py
@@ -9,6 +9,16 @@ class TestTrack(unittest.TestCase):
         self.track1 = Track(1, "Song A", "Artist X")
         self.track2 = Track(2, "Song B", "Artist Y")
 
+    def test_update_track(self):
+        self.track1.track_title = "Song A Updated"
+        self.track1.artist = "Artist Z"
+        self.assertEqual(self.track1.track_title, "Song A Updated")
+        self.assertEqual(self.track1.artist, "Artist Z")
+
+    def test_show_detail(self):
+        detail = f"Track: {self.track1.track_title}, Artist: {self.track1.artist}"
+        self.assertEqual(detail, "Track: Song A, Artist: Artist X")
+
     def test_create_track(self):
         self.assertEqual(self.track1.track_title, "Song A")
         self.assertEqual(self.track2.artist, "Artist Y")
@@ -23,27 +33,13 @@ class TestPlaylist(unittest.TestCase):
         self.playlist.tracks.append(self.track2)
         self.assertIn(self.track2, self.playlist.tracks)
 
-    def test_delete_track(self):
-        self.playlist.tracks.append(self.track2)
-        self.playlist.tracks.append(self.track1)
-        self.playlist.tracks.remove(self.track1)
-        self.assertNotIn(self.track1, self.playlist.tracks)
-
-    def test_update_track(self):
-        self.track1.track_title = "Song A Updated"
-        self.track1.artist = "Artist Z"
-        self.assertEqual(self.track1.track_title, "Song A Updated")
-        self.assertEqual(self.track1.artist, "Artist Z")
-
+    
     def test_clear_playlist(self):
         self.playlist.tracks.append(self.track2)
         self.playlist.tracks.clear()
         self.assertEqual(len(self.playlist.tracks), 0)
 
-    def test_show_detail(self):
-        detail = f"Track: {self.track1.track_title}, Artist: {self.track1.artist}"
-        self.assertEqual(detail, "Track: Song A, Artist: Artist X")
-
+    
 class TestFavorite(unittest.TestCase):
     def setUp(self):
         self.track1 = Track(1, "Song A", "Artist X")
@@ -67,7 +63,7 @@ class TestMusicPlayer(unittest.TestCase):
         self.track1 = Track(1, "Song A", "Artist X")
         self.musicplayer = MusicPlayer()
 
-    def test_play_pause_music(self):
+    def test_play_stop_music(self):
         self.musicplayer.play_track(self.track1)
         self.assertEqual(self.musicplayer.current_track, self.track1)
         self.musicplayer.stop_track()
